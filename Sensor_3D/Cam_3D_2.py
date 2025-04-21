222d6d260486b5f26ae8a493f5f87000f6f5441e
Jefferson Amaya <jefferson.amaya@colsein.com.co>
2025-04-21 12:31:13 -0500

    prueba 3_ se agrega el reinicio de puertos.

--- Sensor_3D/Cam_3D_2.py
+++ Sensor_3D/Cam_3D_2.py
@@ -81,7 +81,30 @@ def read_frame(ser):
             except Exception as e:
                 print("❌ Error procesando frame:", e)
 
+            
+def reiniciar_puerto():
+    print("🚀 Reiniciando puerto serial...")
+    ports = serial.tools.list_ports.comports()
+    available_ports = [p.device for p in ports if 'USB' in p.device]
+    
+    if not available_ports:
+        print("❌ No se encontraron puertos USB disponibles.")
+        return None
+
+    # Intentar cada puerto disponible
+    for port in available_ports:
+        try:
+            ser = serial.Serial(port, 115200, timeout=1)
+            ser.close()  # Liberar el puerto
+            print(f"🔄 Puerto liberado: {port}")
+            return port
+        except Exception as e:
+            print(f"❌ Error al cerrar {port}: {e}")
+    return None
+
 def main():
+    reiniciar_puerto()
+    print(f"✅ Conectado a {port}")
     with serial.Serial(PORT, BAUDRATE, timeout=TIMEOUT) as ser:
         print(f"Conectado a {PORT}")
         send_commands(ser)
